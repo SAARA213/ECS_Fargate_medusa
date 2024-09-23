9f5bbf4506caf4e1ee8f348be5462146de2424c6
Saaradha Kumar <163541698+SAARA213@users.noreply.github.com>
2024-09-23 10:26:56 +0530

    Update variables.tf

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,19 +1,3 @@
-variable "vpc_cidr" {
-    default = "10.0.0.0/16"
-}
-
-variable "public_sb1_cidr" {
-    default = "10.0.0.0/24"
-}
-
-variable "private_sb1_cidr" {
-    default = "10.0.1.0/24"
-}
-
-variable "public_sb2_cidr" {
-    default = "10.0.2.0/24"
-}
-
-variable "private_sb2_cidr" {
-    default = "10.0.3.0/24"
+variable "region" {
+  default = "us-east-1"
 }

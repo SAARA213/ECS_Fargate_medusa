a14acdb7d20c26dcb3dea4cac69857a1117d369b
Saaradha Kumar <163541698+SAARA213@users.noreply.github.com>
2024-09-27 13:26:29 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,5 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 provider "aws" {
-  region = "ap-south-1"  # Change to your preferred region
+  region = "us-east-1"  # Change to your preferred region
 }
 
 resource "aws_vpc" "medusa_vpc" {
@@ -15,9 +24,10 @@ resource "aws_internet_gateway" "medusa_internet_gateway" {
   vpc_id = aws_vpc.medusa_vpc.id
 }
 
-resource "aws_security_group" "medusa_ecs_sg" {
-  name        = "medusa-ecs-sg"
+resource "aws_security_group" "medusa_security_group" {
+  name        = "medusa-sg"
   description = "Allow inbound traffic for Medusa ECS Fargate"
+  vpc_id      = aws_vpc.medusa_vpc.id
 
   ingress {
     from_port   = 9000
@@ -32,15 +42,13 @@ resource "aws_security_group" "medusa_ecs_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  vpc_id = aws_vpc.medusa_vpc.id
 }
 
-resource "aws_ecs_cluster" "medusa_ecs_cluster" {
+resource "aws_ecs_cluster" "medusa_cluster" {
   name = "medusa-ecs-cluster"
 }
 
-resource "aws_ecs_task_definition" "medusa_task_definition" {
+resource "aws_ecs_task_definition" "medusa_task" {
   family                   = "medusa-task"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
@@ -59,28 +67,35 @@ resource "aws_ecs_task_definition" "medusa_task_definition" {
     environment = [
       {
         name  = "DATABASE_URL"
-        value = "your-postgresql-database-url"  # Replace with your DB URL
+        value = "your-postgresql-database-url"
       },
       {
         name  = "JWT_SECRET"
-        value = "your-jwt-secret"  # Replace with your JWT secret
+        value = "your-jwt-secret"
       }
     ]
   }])
 }
 
-resource "aws_ecs_service" "medusa_ecs_service" {
+resource "aws_ecs_service" "medusa_service" {
   name            = "medusa-service"
-  cluster         = aws_ecs_cluster.medusa_ecs_cluster.id
-  task_definition = aws_ecs_task_definition.medusa_task_definition.arn
+  cluster         = aws_ecs_cluster.medusa_cluster.id
+  task_definition = aws_ecs_task_definition.medusa_task.arn
   desired_count   = 1
   launch_type     = "FARGATE"
 
   network_configuration {
     subnets         = [aws_subnet.medusa_public_subnet.id]
-    security_groups = [aws_security_group.medusa_ecs_sg.id]
+    security_groups = [aws_security_group.medusa_security_group.id]
     assign_public_ip = true
   }
 }
+
+output "ecs_service_arn" {
+  value = aws_ecs_service.medusa_service.id
+}
+
+
+
 
   
